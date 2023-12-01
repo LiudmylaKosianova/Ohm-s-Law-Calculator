e21d2f27e49315ac19cfcc6bd2dc5fc85dc6d2b5
snowwhitesheep <snw.wht.lk@gmail.com>
2023-12-01 20:28:51 +0100

    Ohm

--- Ohm'sLawCalculator.c
+++ Ohm'sLawCalculator.c
@@ -2,5 +2,11 @@
 
 int main(){
 
+    printf("Welcome to the Ohm's Law Calculator! Enter R and I, get V and P.\n\n");
+    printf("\tEnter your resistance in Ohms: \n");
+    printf("\tEnter your current in Amps: \n");
+    printf("Press any key to do another calculation or [q] to quit: \n");
+
+
     return 0;
 }
